4cf1b8bf695bba4a4454e964f395d82e58483a01
@nahueadev <nahueadev@gmail.com>
2021-05-09 21:13:39 -0300

    Change E-1.4

--- practica-1/E-1.4.py
+++ practica-1/E-1.4.py
@@ -2,10 +2,9 @@
 
 def calcula_factorial(numero):
     total_factorial=1
-    i=1
     for i in range(1,numero+1):
         total_factorial=total_factorial * i
     return total_factorial
 
 
-print calcula_factorial(10)
+print calcula_factorial(5)
